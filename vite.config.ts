ea78c1692ced713222ea15f1a045503f9aacaca5
Iain MacDonald <IJMacD@gmail.com>
2023-09-28 16:04:14 +0800

    Build

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/shipping-forecast-alarm/",
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          dashjs: ["dashjs"]
+        }
+      }
+    }
+  }
 })
